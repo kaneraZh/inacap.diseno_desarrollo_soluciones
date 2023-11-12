b4401c1e1c889d1083e27c8bcc92235526c6342f
kaneraZh <ari1337ferrada@gmail.com>
2023-11-12 17:39:08 -0300

    correccion de base de datos
    avances buenos, la base de datos tenía un formato que difería con el declarado en el modelo de tablas, igual hay que seguir corrigiendolo, tiene weas muy basicas que le faltan xd

--- salon_de_belleza/punto_venta/models.py
+++ salon_de_belleza/punto_venta/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class UsuarioEmpleado(models.Model):
+class Persona(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     primer_nombre = models.CharField(max_length=20)
     segundo_nombre = models.CharField(max_length=20)
@@ -11,31 +11,15 @@ class UsuarioEmpleado(models.Model):
     fecha_nacimiento = models.DateField()
     correo_electronico = models.EmailField(max_length=30)
     contrasena = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=20)    
+class Empleado(Persona):
     documento_identificador = models.CharField(max_length=30)
     fecha_contratacion = models.DateField()
-
-
-class UsuarioJefe(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    primer_nombre = models.CharField(max_length=20)
-    segundo_nombre = models.CharField(max_length=20)
-    primer_apellido = models.CharField(max_length=20)
-    segundo_apellido = models.CharField(max_length=20)
-    fecha_nacimiento = models.DateField()
-    correo_electronico = models.EmailField(max_length=30)
-    contrasena = models.CharField(max_length=20)
-    documento_identificador = models.CharField(max_length=30)
-    fecha_contratacion = models.DateField()
-
-
-class Cliente(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    primer_nombre = models.CharField(max_length=20)
-    segundo_nombre = models.CharField(max_length=20, blank=True, null=True)
-    primer_apellido = models.CharField(max_length=20)
-    segundo_apellido = models.CharField(max_length=20, blank=True, null=True)
-    fecha_nacimiento = models.DateField()
-
+    afp = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=30)
+    servicios = models.ManyToManyField(Servicio)
+class Cliente(Persona):
+    pass
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=30)
@@ -43,13 +27,13 @@ class Producto(models.Model):
     precio_compra = models.DecimalField(max_digits=10, decimal_places=2)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-
+    proveedor = models.ForeignKey(Proveedor)
 
 class Servicio(models.Model):
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=50)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-
+    tiempo = models.TimeField()
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=40)
@@ -58,12 +42,12 @@ class Proveedor(models.Model):
     telefono_celular = models.CharField(max_length=30)
     email = models.EmailField(max_length=40)
 
-
 class Cita(models.Model):
     fecha = models.DateField()
     hora = models.TimeField()
     servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado)
 
 
 class Boleta(models.Model):
@@ -74,19 +58,19 @@ class Boleta(models.Model):
     )
 
     tipo_de_pago = models.CharField(max_length=25, choices=TIPO_DE_PAGO_CHOICES)
-    productos = models.ManyToManyField(Producto)
-    servicios = models.ManyToManyField(Servicio)
     precio_total = models.DecimalField(max_digits=10, decimal_places=2)
     fecha_y_hora = models.DateTimeField(auto_now_add=True)
     usuario_jefe = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boletas_jefe')
     usuario_empleado = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boletas_empleado')
-
     class Meta:
         permissions = [
             ("can_add_boleta", "Puede agregar boletas"),
             ("can_delete_boleta", "Puede eliminar boletas"),
             ("can_update_boleta", "Puede actualizar boletas"),
         ]
+class Boleta_producto(models.Model):
+    def __str__(self): return self.name
+    
 
 
 class Factura(models.Model):
