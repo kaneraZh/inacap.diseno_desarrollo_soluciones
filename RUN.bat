16cc00d1ab46e63b676d3565a380d5a676d98789
kaneraZh <ari1337ferrada@gmail.com>
2023-12-13 08:07:28 -0300

    init

---
+++ RUN.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\path\to\your\env\scripts & activate & cd /d C:\path\to\your\env\[projectname] & python manage.py runserver"
--- setup.bat
+++ setup.bat
@@ -1,6 +1,14 @@
+winget install Python.Python.3.11
+y
 python -m venv .venv
 .\venv\Scripts\activate.bat
 python.exe -m pip install --upgrade pip
 pip install django
 pip install pillow
 python .\manage migrate
+python .\manage createsuperuser
+admin
+
+password
+password
+y
