491b5b408efa3656781d6b6f3a8aab1e11f07ddb
kaneraZh <ari1337ferrada@gmail.com>
2023-12-15 17:38:43 -0300

    permissions
    auto permissions attempts

--- RUN.bat
+++ RUN.bat
@@ -1,2 +1,1 @@
-@echo off
-cmd /k "cd /d C:\path\to\your\env\scripts & activate & cd /d C:\path\to\your\env\[projectname] & python manage.py runserver"
+cmd /k "python manage.py runserver"
--- punto_venta/apps.py
+++ punto_venta/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+#from django.db.models.signals import post_migrate
 
 class PuntoVentaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'punto_venta'
+#    def ready(self):
+#        from .signals import crear_grupos
+#        post_migrate.connect(crear_grupos, sender=self)
